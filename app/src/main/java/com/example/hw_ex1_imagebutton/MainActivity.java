bb48fb02597cbbeb5d4d1c708ad826cdbe542d54
ml3424 <ml3424@bs.amalnet.k12.il>
2024-01-09 09:41:27 +0200

    not working

--- app/src/main/java/com/example/hw_ex1_imagebutton/MainActivity.java
+++ app/src/main/java/com/example/hw_ex1_imagebutton/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.hw_ex1_imagebutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView iv;
+    ImageButton imBtn1;
+    Random rnd = new Random();
+    int num = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iv = findViewById(R.id.iv);
+        imBtn1 = findViewById(R.id.imBtn1);
+    }
+
+    public void click(View view) {
+        num = rnd.nextInt(3)+1;
+        if(num == 1)
+        {
+            iv.setImageResource(R.drawable.hellokitty1);
+            imBtn1.setImageResource(R.drawable.one);
+        }
+        else if(num == 2)
+        {
+            iv.setImageResource(R.drawable.hellokitty2);
+            imBtn1.setImageResource(R.drawable.two);
+        }
+        else
+        {
+            iv.setImageResource(R.drawable.hellokitty3);
+            imBtn1.setImageResource(R.drawable.three);
+        }
     }
 }
